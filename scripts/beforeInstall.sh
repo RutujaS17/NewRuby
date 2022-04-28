148f87f22c1583c09c2c5af19bfe3bd35be02173
RutujaS17 <91672267+RutujaS17@users.noreply.github.com>
2022-04-28 15:52:53 +0530

    Update beforeInstall.sh

--- scripts/beforeInstall.sh
+++ scripts/beforeInstall.sh
@@ -1,9 +1,15 @@
 #!/usr/bin/bash
-
-rm -rf /var/www/ruby
-git clone https://github.com/JD4912/ruby_test.git /var/www/ruby
+#rm -rf /var/www/ruby
+#git clone https://github.com/JD4912/ruby_test.git /var/www/ruby
 # cd /var/www/ruby && rm Gemfile.lock 
-cd /var/www/ruby/ && rm Gemfile.lock
-gem install bundler 
-bundle install
+#cd /var/www/ruby/ && rm Gemfile.lock
+#gem install bundler 
+#bundle install
+
+rm -rf /home/ubuntu/ruby
+git clone https://github.com/Vaibhavi-07/ruby_test.git /home/ubuntu/ruby
+# sudo chmod 755
+cd /home/ubuntu/ruby
+rm Gemfile.lock
+# bundle install
 
